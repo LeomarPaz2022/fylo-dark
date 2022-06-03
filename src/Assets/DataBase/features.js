63ab3d19f3c52d8397673e3c88d92f80573dee94
LeomarPaz2022 <leomaravpaz@gmail.com>
2022-06-03 00:23:39 -0300

    instalando cra e configurando projeto

---
+++ src/Assets/DataBase/features.js
@@ -0,0 +1,28 @@
+const features = [
+    {
+        img: "./images/icon-access-anywhere.svg",
+        title: "Access your files, anywhere",
+        paragraph: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
+    },
+
+    {
+        img: "./images/icon-security.svg",
+        title: "Security you can trust" ,
+        paragraph: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
+    },
+
+    {
+        img: "./images/icon-collaboration.svg",
+        title: "Real-time collaboration" ,
+        paragraph: "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required."
+    },
+
+    {
+        img: "./images/icon-any-file.svg",
+        title: "Store any type of file" ,
+        paragraph: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
+    },
+]
+
+
+
---
+++ src/Assets/DataBase/testimonials.js
@@ -0,0 +1,25 @@
+const testimonials = [
+    {
+       paragraph: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+       img: "./images/profile-1.jpg",
+       name:"Satish Patel" ,
+       jobFunction: "Founder & CEO, Huddle ",
+
+    },
+
+    {
+        paragraph: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+        img: "./images/profile-2.jpg",
+        name: "Bruce McKenzie",
+        jobFunction: "Founder & CEO, Huddle ",
+ 
+     },
+
+     {
+        paragraph: "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+        img: "./images/profile-3.jpg",
+        name: "Iva Boyd",
+        jobFunction: "Founder & CEO, Huddle ",
+ 
+     },
+]
